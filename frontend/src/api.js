e7aec5be6caddaef838621772dc146c728734649
Carolinaribeirodacol <carolinaribeirodacol@gmail.com>
2023-07-18 21:58:36 -0300

    apontando certo

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 export function getStrapiURL(path = "") {
-    return `${process.env.API_URL}${path}`
+    return `${process.env.REACT_APP_API_URL}${path}`
 }
 
 export function getStrapiAPIURL(path = "") {
